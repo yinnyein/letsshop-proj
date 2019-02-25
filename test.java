2cc224a2dc924b2389f7fc4d6234e37e46d90b26
yinnyein <yinthu1996@gmail.com>
2019-02-26 02:19:22 +0630

    Letsshop 1st commit

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+	public static void main(String args[]) {
+		  for(int i = 0; i < 3; i++) {
+			for(int j = 3; j >= 0; j--) {
+			    if(i == j) continue;
+			    System.out.println(i + " " + j);
+			}
+		    }
+	 	}
+	}
